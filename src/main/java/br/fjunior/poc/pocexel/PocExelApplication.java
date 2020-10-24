d9e0a800bcbaff3cb76b959f3efdba0a3c5d0860
Valmar Júnior <valmar.junior1@gmail.com>
2020-10-24 10:14:58 -0300

    commit initial

---
+++ src/main/java/br/fjunior/poc/pocexel/PocExelApplication.java
@@ -0,0 +1,13 @@
+package br.fjunior.poc.pocexel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocExelApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run( PocExelApplication.class, args );
+	}
+	
+}
---
+++ src/test/java/br/fjunior/poc/pocexel/PocExelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fjunior.poc.pocexel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocExelApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
