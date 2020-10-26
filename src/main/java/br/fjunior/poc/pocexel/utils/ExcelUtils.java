34e4e4d7e259783511e3743d1d58b1925f5452a1
Valmar Júnior <valmar.junior1@gmail.com>
2020-10-26 07:33:05 -0300

    Starting coding for POC

--- src/main/java/br/fjunior/poc/pocexel/utils/ExcelUtils.java
+++ src/main/java/br/fjunior/poc/pocexel/utils/ExcelUtils.java
@@ -1,6 +1,7 @@
 package br.fjunior.poc.pocexel.utils;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.streaming.SXSSFSheet;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.springframework.stereotype.Component;
@@ -28,21 +29,29 @@ public class ExcelUtils {
 			
 			SXSSFSheet lineChartSheetTab = xssfWorkbook.createSheet( "lineChartSheet" );
 			
-			
+			// Max rows supported by Exel: 1.048.576 rows
 			for (int i = 0; i < 1048576; i++) {
 				
 				Cell cell = lineChartSheetTab
 					            .createRow( i )
 					            .createCell( 0 );
 				
-				cell.setCellValue( new BigDecimal( new Random().nextInt() ).intValue() );
+				cell.setCellValue( new BigDecimal( new Random().nextInt( 100000 ) ).intValue() );
 			}
+			
+			int numOfColumns = lineChartSheetTab.getRow( 0 ).getPhysicalNumberOfCells();
+			
+			lineChartSheetTab.setAutoFilter( new CellRangeAddress( 0, 0, 0, 0 );
 			
 			xssfWorkbook.write( outputStream );
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private void populateRows() {
+		new Random().nextInt( 100000 );
 	}
 	
 }
